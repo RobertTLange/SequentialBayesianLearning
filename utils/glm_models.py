67287c2b18b4fd86e497707a60f40c659baef59f
Robert Lange <robert.lange@barcelonagse.eu>
2019-01-27 16:57:53 +0100

    Log 27/01/19 - Train all models on all blocks

--- utils/glm_models.py
+++ utils/glm_models.py
@@ -1,29 +1,50 @@
 import pymc3 as pm
+from pymc3.variational.callbacks import CheckParametersConvergence
 
 
-def run_model_estimation(y_elec, surprise,
-                         model_type, num_traces):
-    data = dict(y_elec=y_elec, surprise=PS)
+def run_model_estimation(y_elec, surprise, model_type):
+    data = dict(y_elec=y_elec, surprise=surprise)
 
     if model_type == "OLS":
-        model = OLS_model(data, num_traces)
+        model = OLS_model(y_elec, surprise)
+    else:
+        raise "Provide a valid model type"
 
-    with neural_network:
+    with model:
         inference = pm.ADVI()
-        approx = pm.fit(n=30000, method=inference)
-    return
+        approx = pm.fit(method=inference,
+                        callbacks=[pm.callbacks.CheckParametersConvergence(diff='absolute')],
+                        n=30000,
+                        progressbar=0)
+    # return full optimization trace of free energy
+    return -approx.hist
 
 
-def OLS_model(data, num_traces):
+def OLS_model(y_elec, surprise):
+    data = dict(y_elec=y_elec, surprise=surprise)
     with pm.Model() as mdl_ols:
-        ## define Normal priors to give Ridge regression
+        # Define Normal priors to give Ridge regression
         b0 = pm.Normal('b0', mu=0, sd=100)
         b1 = pm.Normal('b1', mu=0, sd=100)
 
-        ## define Linear model
+        # Define Linear model
         y_est = b0 + b1 * data['surprise']
 
-        ## Define Normal likelihood with HalfCauchy noise (fat tails, equiv to HalfT 1DoF)
+        # Define Normal likelihood with HalfCauchy noise (fat tails, equiv to HalfT 1DoF)
         sigma_y = pm.HalfCauchy('sigma_y', beta=10)
-        likelihood = pm.Normal('likelihood', mu=y_est, sd=sigma_y, observed=df_lin['y_elec'])
+        likelihood = pm.Normal('likelihood', mu=y_est, sd=sigma_y, observed=data['y_elec'])
     return mdl_ols
+
+
+def Hierarchical_model(y_elec, surprise):
+    data = dict(y_elec=y_elec, surprise=surprise)
+
+    with pm.Model() as mdl_hierarchical:
+        eta = pm.Normal('eta', 0, 1, shape=y_elec.shape[0])
+        mu = pm.Normal('mu', 0, 1e6)
+        tau = pm.HalfCauchy('tau', 5)
+
+        theta = pm.Deterministic('theta', mu + tau*eta)
+
+        obs = pm.Normal('obs', theta, sigma, observed=y)
+    return mdl_hierarchical
